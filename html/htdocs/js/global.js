746987408e76ccc976b5603235cda4ae7bfc7356
Aihara Noriyoshi <aihara@nori44.jp>
2015-06-17 02:05:09 +0900

    - add program text at today's class - clean html directory

---
+++ html/htdocs/js/global.js
@@ -0,0 +1,18 @@
+﻿// documentの読み込み後に実行
+$(document).ready(function(){
+	initPagetop();
+	iOSLabelBugfix();
+});
+
+// ページトップのアニメーション
+var initPagetop = function(){
+	$("a[href^='#_top']").click(function(){
+		$("html,body").animate({ scrollTop:0 }, 600, 'swing', function(){});
+		return false;
+	});
+}
+
+// iOS用のlabelバグフィックス
+var iOSLabelBugfix = function(){
+	$("label").click(function(){});
+}
